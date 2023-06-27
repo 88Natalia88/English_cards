83af1bf45bb24a55ace6b3020383a525fd86bf48
Natalia Balashova <88karamelka88@mail.ru>
2023-06-27 10:33:00 +0300

    add change input

--- cards-app/src/components/ListCards/TableRow.jsx
+++ cards-app/src/components/ListCards/TableRow.jsx
@@ -8,8 +8,10 @@ import FavoriteButton from '../ButtonFavorite/ButtonFavotite';
 
 function TableRow(props) {
     const { english, transcription, russian } = props.word;
+    const [dataValue, setDataValue] = useState(props.word);
     const [pressed, setPressed] = useState(false);
-    const handleEdit = () => {
+
+    const handleEdit = () => { 
     setPressed(!pressed);
 }
 const handleSave = () => {
@@ -20,9 +22,9 @@ const handleCansel = () => {
 }
     return (
         <tr>
-            <td>{pressed ? <input type="text" value={english} /> : english}</td>
-            <td>{pressed ? <input type="text" value={transcription} /> : transcription}</td>
-            <td>{pressed ? <input type="text" value={russian} /> : russian}</td>
+            <td>{pressed ? <input type="text" value={dataValue.english} onChange={e => setDataValue(e.target.value)} /> : english}</td>
+            <td>{pressed ? <input type="text" value={dataValue.transcription} onChange={e => setDataValue(e.target.value)} /> : transcription}</td>
+            <td>{pressed ? <input type="text" value={dataValue.russian} onChange={e => setDataValue(e.target.value)} /> : russian}</td>
             <td>
             {pressed ? (<SaveButton onClick={handleSave}/>) : (<EditButton onClick={handleEdit}/>)}
             <FavoriteButton/>
